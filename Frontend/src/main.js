38209444c259dbe5ddd7c47bbfc18effa3472dfa
AnnaMitsan <annamitsan@gmail.com>
2016-12-13 23:50:05 +0200

    нет сервера, рецепты грузятся из json

---
+++ Frontend/src/main.js
@@ -0,0 +1,6 @@
+$(function(){
+    //This code will execute when the page is ready
+    var RecipeMenu = require('./recipe/RecipeMenu');
+  
+    RecipeMenu.initialiseMenu();
+});
---
+++ Frontend/src/recipe/RecipeMenu.js
@@ -0,0 +1,28 @@
+var Templates = require('../Templates');
+var Recipe_List = require('../recipe_list');
+var $recipe_list = $("#recipe_list");
+
+function showRecipeList(list) {
+    $recipe_list.html("");
+
+    function showOneRecipe(recipe) {
+        var html_code = Templates.RecipeList_OneItem({recipe: recipe});
+
+        var $node = $(html_code);
+        console.log(recipe.name);
+
+        $node.find(".more").click(function(){
+            
+        });
+        
+        $recipe_list.append($node);
+    }
+
+    list.forEach(showOneRecipe);
+}
+
+function initialiseMenu() {
+    showRecipeList(Recipe_List); 
+}
+
+exports.initialiseMenu = initialiseMenu;
